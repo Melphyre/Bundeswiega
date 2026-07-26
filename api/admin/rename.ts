f68fd4db01ac55b13f505eb543ddc2f1f5b5dd82
Melphyre <mitternachtklaus@gmail.com>
2026-07-26 16:53:34 +0200

    Add files via upload

--- api/admin/rename.ts
+++ api/admin/rename.ts
@@ -1,6 +1,8 @@
 import { put, list } from "@vercel/blob";
 
 export default async function handler(req: any, res: any) {
+  res.setHeader("Content-Type", "application/json");
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed. Use POST." });
   }

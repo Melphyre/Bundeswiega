1792063191f2ba08d68063253517e0c4168e4beb
Melphyre <mitternachtklaus@gmail.com>
2026-09-07 16:34:26 +0200

    Add files via upload

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
 import { Player, ParsedRecord } from '../types';
 import { TOGETHER_ACHIEVEMENT_IDS } from '../utils';
 export { MASTER_ACHIEVEMENTS_DEFINITIONS } from './achievementsData';
+export * from './constants/titlesConfig';
 
 export const LOGO_URL = "https://github.com/Melphyre/Bundeswiega/blob/main/Bundeswiega.png?raw=true";
 export const INSTAGRAM_URL = "https://www.instagram.com/bundeswiega/";
